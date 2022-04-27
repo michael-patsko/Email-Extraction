98722114045e23f350e13225b97f8f9864dc2765
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-27 12:08:49 +0100

    modified code to use RegEx for searching text file

--- email.js
+++ email.js
@@ -1,17 +1,11 @@
 var counter = 0;
+const regex = /\b\w*@softwire\.com\b/gi;
 
 const fs = require('fs');
-fs.readFile('test.txt', 'utf-8', (err,data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    for (let i = 0; i < data.length; i++) {
-        if ((data.substring(i,i+14) === '@softwire.com ')) {
-            counter += 1;
-            j = data.lastIndexOf(" ", i);
-            console.log(data.substring(j+1,i+13));
-        }
-    }
-    console.log(`There are ${counter} Softwire emails.`);
-});
+const textFile = fs.readFileSync('test.txt', 'utf-8');
+
+const matchesArray = textFile.match(regex);
+for (let i = 0; i < matchesArray.length; i++) {
+    console.log(matchesArray[i])
+}
+console.log(`There are ${matchesArray.length} Softwire emails.`)
