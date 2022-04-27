b4f8dff7e01e28983230d016d3e2a4e307a3292e
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-04-27 14:55:00 +0100

    modified code to display 10 most common domains as well as domains with a frequency above a given number

--- email.js
+++ email.js
@@ -1,3 +1,8 @@
+// Care about top-level domain?
+
+// Email frequency: 
+domainFreq = 150 ;
+
 const fs = require('fs');
 const input = fs.readFileSync('test.txt','utf-8');
 const emailRegex = /\b\S+@\S+\.\S+\b/gi;
@@ -5,6 +10,7 @@ const domainRegex = /(?<=\S+@)\S+\.\S+\b/gi;
 emailMatchArray = input.match(emailRegex);
 domainMatchArray = input.match(domainRegex);
 
+// Adds email domains to a dictionary with their respective frequencies
 var domains = {};
 for (let i = 0; i < domainMatchArray.length; i++) {
     if (domainMatchArray[i] in domains) {
@@ -14,4 +20,19 @@ for (let i = 0; i < domainMatchArray.length; i++) {
     }
 }
 
-console.log(domains);
+domainsArray = [];
+
+// Create domainsArray for sorting purposes
+for (var key in domains) {
+    domainsArray.push([key,domains[key]]);
+}
+
+domainsArray.sort((a,b) => b[1] - a[1]);
+
+console.log("The 10 most common domains are: ");
+for (let i = 0; i < 10; i++) console.log(`${domainsArray[i][0]}: ${domainsArray[i][1]} occurences`);
+
+console.log(`\nThe domains with frequencies above ${domainFreq} are: `);
+for (let i = 0; i < domainsArray.length; i++) {
+    if (domainsArray[i][1] >= domainFreq) console.log(`${domainsArray[i][0]}: ${domainsArray[i][1]} occurences`);
+}
